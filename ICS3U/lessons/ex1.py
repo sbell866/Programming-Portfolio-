eb46687bb46e8a1db111c8fd23370bc35b90a1a6
Sam Bell <bellsam590@gmail.com>
2026-09-17 14:06:20 -0400

    commit5

---
+++ ICS3U/lessons/ex1.py
@@ -0,0 +1,8 @@
+print ("hello world")
+print ("I like typing this.")
+print ("this is fun.")
+#ex1.py,line 2, SyntaxError: unterminated string literal (detected at line 2)
+print ("3 + 4")
+print (3 + 4)
+print ()
+#think check: "3 + 4" is a string, while 3 + 4 is an expression, meaning that 3 + 4 will equal 7, while "3 + 4" will equal "3 + 4"
