0671cd7a0a8972dffc857d49615df4bb1f88353d
Sam Bell <bellsam590@gmail.com>
2026-09-18 14:13:14 -0400

    commit7

---
+++ ICS3U/lessons/lesson3.py
@@ -0,0 +1,7 @@
+food=input("What is your favorite food? ")
+(print)("your favorite food is",food)
+age=input("How old are you? ")
+(print)("you are",age,"years old")
+#asking user favorite colour and then printing it
+colour=input("what is your favorite colour ")
+(print)(colour,"is your favorite colour")
