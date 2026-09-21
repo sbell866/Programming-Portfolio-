90849989d832d63811a7c25db02e14f2f296edcf
Sam Bell <bellsam590@gmail.com>
2026-09-21 14:04:50 -0400

    commit9

---
+++ ICS3U/lessons/lesson4.py
@@ -0,0 +1,7 @@
+name = "Sam"
+age = 16
+print("My name is", name, "and I'm", age, "years old")
+colour = input("What's your favorite colour? ")
+print("Your favorite colour is:", colour)
+number = int(input("Pick any number: "))
+print("Your number is:", number)
---
+++ ICS3U/lessons/lesson5.py
@@ -0,0 +1,8 @@
+word = "sword"
+(print)(word)
+age = 16
+(print)(age, "plus 5 is", age + 5)
+decimal = 1.5
+(print)(decimal, "times 10 is", decimal * 10)
+number = int(input("Pick any number: "))
+(print)(number, "times 10 times 2 is", number * 10 * 2)
