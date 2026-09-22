45fb87623c5fa4b8caff1ead3f71e4ce65d85f5f
Sam Bell <bellsam590@gmail.com>
2026-09-22 14:01:11 -0400

    commit10

---
+++ ICS3U/lessons/oop1.py
@@ -0,0 +1,8 @@
+name = "Bella"
+grade = "10"
+print(name, "is in grade", grade)
+names = ["Bella", "Ahmed", "Sarah"]
+grades = ["10", "11", "12"]
+print(names[0], "is in grade", grades[0])
+print(names[1], "is in grade", grades[1])
+print(names[2], "is in grade", grades[2])
