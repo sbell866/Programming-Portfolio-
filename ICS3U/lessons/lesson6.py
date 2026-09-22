fc22e554c4953186f0cd4cf24c7fa72f45e8fcab
Sam Bell <bellsam590@gmail.com>
2026-09-22 14:21:56 -0400

    commit12

--- ICS3U/lessons/lesson6.py
+++ ICS3U/lessons/lesson6.py
@@ -1,1 +1,4 @@
-place = (input("enter a place: "))
+place = (input("enter any place: "))
+adjective = (input("enter any adjective: "))
+name = (input("enter any name: "))
+animal = (input("enter any animal: "))
